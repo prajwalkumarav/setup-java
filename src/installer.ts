007c1cc127be470c23c7b71e2518e5085de4d749
prajwalkumarav <60998765+prajwalkumarav@users.noreply.github.com>
2020-02-14 12:57:46 +0530

    Update installer.ts

--- src/installer.ts
+++ src/installer.ts
@@ -281,7 +281,6 @@ function normalizeVersion(version: string): string {
   }
 
   if (version.endsWith('-ea')) {
-    
     // convert e.g. 14-ea to 14.0.0-ea
     if (version.indexOf('.') == -1) {
       version = version.slice(0, version.length - 3) + '.0.0-ea';
